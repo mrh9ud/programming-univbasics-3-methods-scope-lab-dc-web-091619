8834c96c840c65622d26b3ae428a850aee3cd8ad
Matthew Heavner <mrh9ud@virginia.edu>
2019-09-04 18:26:13 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,10 +2,16 @@ $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 def mario
   phrase = "It's-a me, Mario!"
-  
+  puts $status
   puts phrase
 end
 
 def toadstool
   puts $status
 end
+
+def link
+  puts "It's Dangerous To Go Alone!"
+end
+
+def any_phrase
